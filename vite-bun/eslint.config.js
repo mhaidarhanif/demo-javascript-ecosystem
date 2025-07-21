5a790d2924a44c02465debe8e8f3e87ef84881bf
M Haidar Hanif <git@mhaidarhanif.com>
2025-07-21 22:24:56 +0700

    feat: setup eslint

---
+++ vite-bun/eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,ts,mts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,ts,mts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+]);
